3adb483e64a09dca87b3894e2fd88ce3938390fc
monzelb <monzelb@gmail.com>
2017-05-03 14:57:08 -0700

    toggled signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
+
+  enum role: {"Friend/Family Member" => 0, "Teacher" => 1, "Admin" => 2}
 end
