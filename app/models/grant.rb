9c1334dfbe2d8479c333737db5f8ccae75f271e7
Logan Medina <loganjmedina@gmail.com>
2017-05-12 16:18:05 -0700

    added enumerables to grants model

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -5,6 +5,16 @@ class Grant < ApplicationRecord
 
   enum status: { "draft" => 0, "pending" => 1, "approved" => 2, "rejected" => 3, "failed" => 4, "successful" =>5}
 
+  enum subject_areas: ['After School Program', 'Arts / Music', 'Arts / Dance', 'Arts / Drama',
+    'Arts / Visual', 'Community Service', 'Computer / Media', 'Computer Science',
+    'Foreign Language / ELL / TWI','Gardening','History & Social Studies / Multi-culturalism',
+    'Mathematics','Multi-subject','Nutrition','Physical Education', 'Reading & Writing / Communication','Science & Ecology',
+    'Special Ed','Student / Family Support / Mental Health']
+
+  enum funds_will_pay_for: ['Supplies','Books','Equipment','Technology / Media',
+    'Professional Guest (Consultant, Speaker, Artist, etc.)','Professional Development',
+    'Field Trips / Transportation','Assembly']
+
   def state_transition(status)
     if      self.draft?     &&  ["pending"].include?(status)
       return true
