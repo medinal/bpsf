7b5cbed917a0efec3efbfa373415f85200efaaec
monzelb <monzelb@gmail.com>
2017-05-12 12:07:09 -0700

    domne with modals

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get "/user/:user_id", to: "users#show"
-
+match '/contact_forms/new',     to: 'contact_forms#new',             via: 'get'
+resources "contact_forms", only: [:new, :create]
   root to: "home#index"
 
 
