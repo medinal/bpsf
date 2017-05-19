fb5fc2f65d959bd13943e8c6932d12bc796643f6
klawton1 <klawton1@gmail.com>
2017-05-19 16:13:56 -0700

    added links for cropper.js. Added functions for showing crop frame(still needs work)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,4 +64,61 @@ $(document).on('turbolinks:load', function() {
     $('#filter-form').submit();
   });
 
+  // FOR READING URL TO DISPLAY IMG FILE
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      console.log('FILE:', input.files[0]);
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#grant-img-preview').attr('src', e.target.result);
+        $('.resize-img').css('display', 'inline');
+        $('#grant-img-preview').cropper({
+          aspectRatio: 16 / 9,
+          background: false,
+          scalable: false,
+          movable: false,
+          zoomable: false
+        });
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#grant_image").on('change', function(e){
+    readURL(this);
+  });
+  // FOR CROPPING GRANT IMG
+  $('#new-grant').on('click', function(e){
+    e.preventDefault();
+    debugger;
+  });
+
+  $('#crop-img').on('click', function(e){
+    e.preventDefault();
+    $("#blur-div").height($(window).height());
+    $("#blur-div").width($(window).width());
+
+  });
+
+  $('#img-button').on('click', function(){
+    $('#resize-img').html(`
+      <img src="#" id="grant-img-preview">
+    `);
+    $('#grant-img-preview').cropper('getCroppedCanvas').toBlob(function (blob) {
+      var name = $('#grant-img')[0].files[0].name.split(".")[0] + Math.floor(Math.random() * 10000000).toString() + ".jpg"
+      var new_img = new File([blob], name, {type: "image/jpeg"});
+      url = URL.createObjectURL(new_img);
+      $('#final-crop').attr('src', url);
+      var formdata = new FormData();
+      var user_id = $('form')[0].id.split("_")[2]
+      formdata.append(name, new_img);
+      formdata.append("name", name)
+      $.ajax({
+        method: "PATCH",
+        url: ("/user/avatar/" + user_id),
+        data: formdata,
+        processData: false,
+        contentType: false
+      });
+    });
+  });
 });
