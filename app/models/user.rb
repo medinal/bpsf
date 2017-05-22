55d5435769260874fbc6a6e3f346391a6f069973
Logan Medina <loganjmedina@gmail.com>
2017-05-22 09:53:39 -0700

    fixed user model to be able to ask for a users school

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
   has_many :payments
-  has_one :school
+  belongs_to :school
   validates :first_name, :last_name, :email, :role, presence: true
 
 
