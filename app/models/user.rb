e798b8ec04030a1c438a21f99ff8986c9a007bfc
Logan Medina <loganjmedina@gmail.com>
2017-06-02 15:34:44 -0700

    removed admin enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :role, presence: true
 
 
-  enum role: {"friends_and_family" => 0, "teacher" => 1, "admin" => 2}
+  enum role: {"friends_and_family" => 0, "teacher" => 1}
 
   def full_name
     first_name + " " + last_name
