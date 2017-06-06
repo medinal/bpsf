af233f67f5665d0d35373cb7aae20bcff4d6a908
klawton1 <klawton1@gmail.com>
2017-06-06 13:43:30 -0700

    merged new payment forms. added checks for donation for returning customers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -138,23 +138,24 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  $('#donateButton').on('click', function(e) {
-    e.preventDefault();
+  $('.donation').on('click', function(e) {
 
     $('#error_explanation').html('');
 
     var amount = $('input#amount').val();
     amount = amount.replace(/\$/g, '').replace(/\,/g, '')
-
     amount = parseFloat(amount);
 
     if (isNaN(amount)) {
+      e.preventDefault();
       $('#error_explanation').html('<p>Please enter a valid amount in USD ($).</p>');
     }
     else if (amount < 5.00) {
+      e.preventDefault();
       $('#error_explanation').html('<p>Donation amount must be at least $5.</p>');
     }
-    else {
+    else if ($('#donateButton')[0]){
+      e.preventDefault();
       amount = amount * 100; // Needs to be an integer!
       handler.open({
         amount: Math.round(amount)
