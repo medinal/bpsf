85f74f5d52f9da4f081c94bad323be9d9d9e141f
klawton1 <klawton1@gmail.com>
2017-06-28 16:47:24 -0700

    remove

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,4 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
