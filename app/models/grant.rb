10b305da56a45e29010d22df5dd9275d5b456ac2
klawton1 <klawton1@gmail.com>
2017-07-29 18:06:32 -0700

    changed enums to class methods that return array

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -6,15 +6,19 @@ class Grant < ApplicationRecord
 
   enum status: { "draft" => 0, "pending" => 1, "approved" => 2, "rejected" => 3, "failed" => 4, "successful" =>5}
 
-  enum subject_areas: ['After School Program', 'Arts / Music', 'Arts / Dance', 'Arts / Drama',
-    'Arts / Visual', 'Community Service', 'Computer / Media', 'Computer Science',
-    'Foreign Language / ELL / TWI','Gardening','History & Social Studies / Multi-culturalism',
-    'Mathematics','Multi-subject','Nutrition','Physical Education', 'Reading & Writing / Communication','Science & Ecology',
-    'Special Ed','Student / Family Support / Mental Health']
+  def subject_areas_options
+    [nil,'After School Program', 'Arts / Music', 'Arts / Dance', 'Arts / Drama',
+      'Arts / Visual', 'Community Service', 'Computer / Media', 'Computer Science',
+      'Foreign Language / ELL / TWI','Gardening','History & Social Studies / Multi-culturalism',
+      'Mathematics','Multi-subject','Nutrition','Physical Education', 'Reading & Writing / Communication',
+      'Science & Ecology','Special Ed','Student / Family Support / Mental Health']
+  end
 
-  enum funds_will_pay_for: ['Supplies','Books','Equipment','Technology / Media',
-    'Professional Guest (Consultant, Speaker, Artist, etc.)','Professional Development',
-    'Field Trips / Transportation','Assembly']
+  def funds_will_pay_for_options
+    [nil,'Supplies','Books','Equipment','Technology / Media',
+      'Professional Guest (Consultant, Speaker, Artist, etc.)','Professional Development',
+      'Field Trips / Transportation','Assembly']
+  end
 
   validate :state_transition, on: :save
   validates :user, :school, :status, presence: true
