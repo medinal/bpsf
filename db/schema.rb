cb70507f0030b90a9f3ad92b831babb06f9f5880
klawton1 <klawton1@gmail.com>
2017-07-30 01:21:00 -0700

    finally removed state from grants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728222141) do
+ActiveRecord::Schema.define(version: 20170730081409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 20170728222141) do
     t.text     "collaborators"
     t.text     "comments"
     t.integer  "user_id"
-    t.string   "state"
     t.string   "video"
     t.string   "image"
     t.integer  "school_id"
