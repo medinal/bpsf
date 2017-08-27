81763cc727b3da34c58235d06050b787a73be495
klawton1 <klawton1@gmail.com>
2017-08-27 13:31:51 -0700

    admin link to view grant and format for profile index

--- app/admin/profile.rb
+++ app/admin/profile.rb
@@ -2,9 +2,28 @@ ActiveAdmin.register Profile do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :about, :address, :city, :position, :state,
+  permit_params :about, :address, :city, :position, :state,
               :zipcode, :grade, :home_phone, :work_phone,
-              :subject, :relationship, :started_teaching
+              :subject, :started_teaching, :user_id
+
+  index do
+    selectable_column
+    column :user_id do |profile|
+      link_to profile.user.full_name, admin_user_path(profile.user)
+    end
+    column :about
+    column :address
+    column :city
+    column :position
+    column :state
+    column :zipcode
+    column :grade
+    column :home_phone
+    column :work_phone
+    column :subject
+    column :started_teaching
+    actions
+  end
 #
 # or
 #
