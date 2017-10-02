9a081c0c940c1d575295b8eee87dccbb42b6e8d1
monzelb <monzelb@gmail.com>
2017-10-01 22:06:48 -0700

    added grants index to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     @grants = Grant.where(status: :approved).order(deadline: :asc)
     @grants = @grants.first(5)
+    @school = School.all
+    @grantslist = Grant.where("status = '2' OR status = '5'").paginate(page: params[:page], per_page: 5).order(deadline: :asc)
   end
 
 end
